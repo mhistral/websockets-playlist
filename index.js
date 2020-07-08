b585340c914ccdc8fca8f311d0c23eab79c0fcd2
kimmy <kimberlyparrenas@gmail.com>
2020-07-08 11:51:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// install npm install nodemon -g
+// run nodemon index to run server to gitbash
+// https://www.youtube.com/watch?v=vQjiN8Qgs3c&list=PL4cUxeGkcC9i4V-_ZVwLmOusj8YAUhj_9
+// install socket.io - npm install socket.io --save
+
+
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server =  app.listen(4000, function(){
+    console.log('listening to request on port 4000');
+});
+
+// Static Files
+
+app.use(express.static('public_html'));
+
+
+// Socket setup
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id)
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    })
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data)
+    })
+});
